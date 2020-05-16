7fac89337f39b161b5a20bf0333ce0ad08682851
J_Shah <46945882+sjdemon@users.noreply.github.com>
2020-05-16 12:12:48 -0400

    Update get.py

--- get.py
+++ get.py
@@ -1,26 +1,18 @@
-import io
-import os
-
-# Imports the Google Cloud client library
 from google.cloud import vision
-from google.cloud.vision import types
 
-# Instantiates a client
+pics = ['https://i.imgur.com/2EUmDJO.jpg', 'https://i.imgur.com/FPMomNl.png']
+
 client = vision.ImageAnnotatorClient()
+image = vision.types.Image()
 
-# The name of the image file to annotate
-file_name = os.path.abspath('resources/wakeupcat.jpg')
+for pic in pics:
+    image.source.image_uri = pic
+    response = client.face_detection(image=image)
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-
-image = types.Image(content=content)
-
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-print('Labels:')
-for label in labels:
-    print(label.description)
+    print('=' * 79)
+    print('File: {pic}')
+    for face in response.face_annotations:
+        likelihood = vision.enums.Likelihood(face.surprise_likelihood)
+        vertices = [f'({v.x},{v.y})' for v in face.bounding_poly.vertices]
+        print(f'Face surprised: {likelihood.name}')
+        print(f'Face bounds: {",".join(vertices)}')
