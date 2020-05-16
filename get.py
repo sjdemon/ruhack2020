924fd1119c3d72a112da3cb8828704753022dc85
J_Shah <46945882+sjdemon@users.noreply.github.com>
2020-05-16 14:19:44 -0400

    A simple class for identify the image

--- get.py
+++ get.py
@@ -4,18 +4,23 @@ import os
 import io
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'KEY.json'
+
 pics = ['https://i.imgur.com/2EUmDJO.jpg', 'https://i.imgur.com/FPMomNl.png',
         'https://images.everydayhealth.com/images/diet-nutrition/how-many-calories-are-in-a-banana-1440x810.jpg']
+
+
 class Identify():
     def __init__(self, pics):
-            self._pics = pics
+        self._pics = pics
+
     def pics(self):
         return self._pics
-    def Run(self):
-        client = vision.ImageAnnotatorClient()
-        image = vision.types.Image()
 
-        for pic in pics:
+    def run(self):
+        client = vision.ImageAnnotatorClient()
+        image = types.Image()
+
+        for pic in self._pics:
             image.source.image_uri = pic
             resp = client.label_detection(image=image)
 
@@ -26,3 +31,7 @@ class Identify():
                 print(label.description)
 
             print('=' * 79)
+
+
+items = Identify(pics)
+items.run()
